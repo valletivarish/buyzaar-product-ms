9326b9e6e05e7a9c608b88970f845d579b9a3845
valletivarish <varishvalleti52@gmail.com>
2025-06-18 20:43:24 +0530

    Add test folder structure

---
+++ src/test/java/com/buyzaar/myapp/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyzaar.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
