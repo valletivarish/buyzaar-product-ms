e0eb3eb46e898e202465f6e677a71204c234c3ba
valletivarish <varishvalleti52@gmail.com>
2025-06-18 20:41:16 +0530

    Add application constants

---
+++ src/main/java/com/buyzaar/product/constants/AppConstants.java
@@ -0,0 +1,19 @@
+package com.buyzaar.product.constants;
+public class AppConstants {
+	public static final String TAG_NAME = "name";
+	public static final String TAG_DESCRIPTION = "description";
+	public static final String TAG_POPULARITY_SCORE = "popularityScore";
+	public static final String TAG_UPDATED_AT = "updatedAt";
+	public static final String TAG_CREATED_AT = "createdAt";
+	public static final String PRODUCT_ID="productId";
+	public static final String PRODUCT_DOES_NOT_EXIST = "Product does not exist with productId: ";
+	public static final String PRODUCT_CATEGORY="category";
+	public static final String PRODUCT_DESCRPTION="description";
+	public static final String TAGIDS="tagIds";
+	public static final String PRODUCT_NAME="name";
+	public static final String PRODUCT_BRAND="brand";
+	public static final String PRODUCT_UPDATED_AT="updatedAt";
+	public static final String PRODUCT_CREATED_AT="createdAt";
+	
+	
+}
