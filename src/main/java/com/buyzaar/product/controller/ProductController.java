aa27d99802793828cfa1ae63768a20c9b70f0760
valletivarish <varishvalleti52@gmail.com>
2025-06-18 20:41:55 +0530

    Add REST controller for product module

---
+++ src/main/java/com/buyzaar/product/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.buyzaar.product.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.buyzaar.product.model.entity.Product;
+import com.buyzaar.product.service.ProductService;
+
+@RestController
+@RequestMapping("/buyzaar/products")
+public class ProductController {
+
+	ProductService productService;
+
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@PostMapping
+	public ResponseEntity<String> saveProduct(@RequestBody Product product) {
+		productService.saveProduct(product);
+		return new ResponseEntity<>("Saved Succesfully", HttpStatus.OK);
+	}
+	
+	@GetMapping("{productId}")
+	public ResponseEntity<Product> getProduct(@PathVariable String productId){
+		return new ResponseEntity<>(productService.getProduct(productId),HttpStatus.OK);
+	}
+	
+	@PatchMapping("{productId}/tags")
+	public ResponseEntity<String> updateTagsForProductId(@PathVariable String productId,@RequestBody List<String> tagIds){
+		productService.updateTagsForProductId(productId,tagIds);
+		return new ResponseEntity<>("Tags updated successfully for the productId :"+productId,HttpStatus.OK);
+		
+	}
+}
---
+++ src/main/java/com/buyzaar/product/controller/TagController.java
@@ -0,0 +1,41 @@
+package com.buyzaar.product.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.buyzaar.product.model.dto.InputRequest;
+import com.buyzaar.product.model.dto.TagDTO;
+import com.buyzaar.product.model.entity.Tag;
+import com.buyzaar.product.service.ProductService;
+import com.buyzaar.product.service.utils.DtoConverter;
+
+@RestController
+@RequestMapping("/buyzaar/products/tags")
+public class TagController {
+	ProductService productService;
+
+	public TagController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Tag>> getAllTags() {
+		List<Tag> tags = productService.getAllTags();
+		return new ResponseEntity<>(tags, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Tag> saveTag(@RequestBody InputRequest<TagDTO> inputRequest){
+		TagDTO tagDto = inputRequest.getRequest();
+		Tag savedTag = productService.saveTag(DtoConverter.convertTagDtoToTag(tagDto));
+		return new ResponseEntity<>(savedTag,HttpStatus.OK);
+	}
+	
+}
