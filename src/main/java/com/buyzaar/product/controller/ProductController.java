64fbe7d532386604e0811ecff3cebb7dce2f4ac1
Valleti Varish <150535998+valletivarish@users.noreply.github.com>
2025-06-23 17:44:15 +0530

    feat(product-controller): support min and max selling price filters via query params

--- src/main/java/com/buyzaar/product/controller/ProductController.java
+++ src/main/java/com/buyzaar/product/controller/ProductController.java
@@ -63,10 +63,14 @@ public class ProductController {
 	public ResponseEntity<List<Product>> getAllProducts(@RequestParam(required = false) String cursorId,
 			@RequestParam(defaultValue = "10") int limit, @RequestParam(required = false) Boolean isPrevious,
 			@RequestParam(required = false) String name, @RequestParam(required = false) String category,
-			@RequestParam(required = false) List<String> tagIds) {
+			@RequestParam(required = false) List<String> tagIds,
+			@RequestParam(required = false, defaultValue = "") String minValue,
+			@RequestParam(required = false, defaultValue = "") String maxValue) {
 		boolean goingBackward = Boolean.TRUE.equals(isPrevious);
-		return new ResponseEntity<>(productService.getAllProducts(cursorId, limit, goingBackward, name, category,tagIds),
-				HttpStatus.OK);
+		double min = minValue.isBlank() ? 0.0 : Double.parseDouble(minValue);
+		double max = maxValue.isBlank() ? Double.MAX_VALUE : Double.parseDouble(maxValue);
+		return new ResponseEntity<>(productService.getAllProducts(cursorId, limit, goingBackward, name, category,
+				tagIds, min, max), HttpStatus.OK);
 	}
 	
 	
